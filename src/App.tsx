811d604351cc03c4bc981b83b8d54ef62fb8a4dd
julifighter96 <131687050+julifighter96@users.noreply.github.com>
2024-12-18 17:12:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/piano-rentals" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
